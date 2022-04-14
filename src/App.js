c2aa9f7f55ae9304f39d82a3f4363f02fc628fba
Salman-S-Rahman <salmanmern@gmail.com>
2022-04-14 23:38:18 +0600

    all routes set up done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home/Home";
+import Header from "./Pages/Shared/Header/Header";
+import Footer from "./Pages/Shared/Footer/Footer";
+import About from "./Pages/About/About";
+import Contact from "./Pages/Contact/Contact";
+import Login from "./Pages/Login/Login";
+import NotFound from "./Pages/NotFound/NotFound";
 
 function App() {
   return (
     <div>
-      <h1>Genius Car Services</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="home" element={<Home />}></Route>
+        <Route path="about" element={<About />}></Route>
+        <Route path="login" element={<Login />}></Route>
+        <Route path="contact" element={<Contact />}></Route>
+        <Route path="*" element={<NotFound/>}></Route>
+      </Routes>
+      <Footer />
     </div>
   );
 }
